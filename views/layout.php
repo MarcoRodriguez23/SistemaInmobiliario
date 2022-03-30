d1b9e636fbddcea91a6e403281a468f738995db1
Marco Rodriguez <cv5233@innovaccion.mx>
2022-03-30 10:46:14 -0600

    intento 1 para corregir interpreteación de acentos en digitalOcean

--- views/layout.php
+++ views/layout.php
@@ -7,7 +7,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf8_decode">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="build/css/app.css">
